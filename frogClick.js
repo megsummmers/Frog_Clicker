4c123484f5c79e98cc012a8c6844c4eb5d964a27
megsummmers <70597858+megsummmers@users.noreply.github.com>
2024-01-05 17:04:03 -0500

    Update

--- frogClick.js
+++ frogClick.js
@@ -9,13 +9,18 @@ function changeBG(){
     frog.offsetTop = frog.offsetTop;
     frog.classList.add("transform");
     //change background
-    if(clickNum == 1 || clickNum == 5 || clickNum == 15 || clickNum == 25 || clickNum == 35 || clickNum == 50 || clickNum == 65){
+    if(clickNum == 1 || clickNum == 5 || clickNum == 10 || clickNum == 20 || clickNum == 30 || clickNum == 45 || clickNum == 60 || clickNum == 75 || clickNum == 90 || clickNum == 105){
         imgNum++;
-    } else if (clickNum == 135){
+    } else if (clickNum == 125){
         frog.src("frogHeart.PNG");
     } else if (clickNum == 150){
         frog.src("frogHappy.PNG");
     }
     document.getElementById("background").src = `bg${imgNum}.PNG`;
     document.getElementById("tracker").innerHTML = "num "+clickNum;
-  }
+}
+
+function changeScreen(){
+    document.getElementById("message").style.display = "none";
+    document.getElementById("main").style.display = "block";
+}
